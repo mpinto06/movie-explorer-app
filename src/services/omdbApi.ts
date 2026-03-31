4e6a01aa1b83c008276c4ef5aa56a9d12d9d3ca7
Miguel Pinto <miguelep0106@gmail.com>
2026-03-31 15:12:17 -0400

    fix: https para netflify

--- src/services/omdbApi.ts
+++ src/services/omdbApi.ts
@@ -1,5 +1,5 @@
 const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
-const BASE_URL = 'http://www.omdbapi.com/';
+const BASE_URL = 'https://www.omdbapi.com/';
 
 export interface Movie {
   Title: string;
